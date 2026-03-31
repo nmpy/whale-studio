0a24f6a59b0fd959a7d3c1045adf35f490b299a9
nmpy <namipoyoo@gmail.com>
2026-03-31 23:40:28 +0900

    fix: force dynamic tracking route

--- src/app/t/[trackingId]/route.ts
+++ src/app/t/[trackingId]/route.ts
@@ -1,4 +1,9 @@
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+export const runtime = 'nodejs'
+
 // src/app/t/[trackingId]/route.ts
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
